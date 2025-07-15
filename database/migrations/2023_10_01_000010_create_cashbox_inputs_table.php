2b4e7fb0bf850ed72a5af456021a0e632561a88a
islamabdurahman <abdurahmanislam304@gmail.com>
2025-07-15 09:07:05 +0500

    orderFile

--- database/migrations/2023_10_01_000010_create_cashbox_inputs_table.php
+++ database/migrations/2023_10_01_000010_create_cashbox_inputs_table.php
@@ -11,10 +11,10 @@ class CreateCashboxInputsTable extends Migration
         Schema::create('cashbox_inputs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cashbox_id')->constrained('cashboxes');
-            $table->bigInteger('user_id');
-            $table->bigInteger('client_id');
-            $table->foreignId('graphic_id')->constrained('graphics');
-            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('client_id')->nullable()->constrained('users');
+            $table->foreignId('graphic_id')->nullable()->constrained('graphics');
+            $table->foreignId('order_id')->nullable()->constrained('orders');
             $table->double('amount');
             $table->string('comment')->nullable();
             $table->timestamps();
